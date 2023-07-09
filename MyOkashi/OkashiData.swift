39c69d63fbda5a102c629f1407d1fa8019c47cde
tommy <tmtmtoa@gmail.com>
2023-07-09 17:13:33 +0900

    JSON取得

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -4,9 +4,22 @@ import Foundation
 
 // お菓子データ検索用クラス
 class OkashiData: ObservableObject {
+    // JSONデータ構造
+    struct ResultJson: Codable {
+        struct Item: Codable {
+            // お菓子の名称
+            let name: String?
+            // 掲載URL
+            let url: URL?
+            // 画像URL
+            let image: URL?
+        }
+        // 複数要素
+        let item: [Item]?
+    }
     // web API検索用メソッド　第一引数:keyword 検索したいワード
     func searchOkashi(keyword: String) {
-        print("searchOkashiメソッドで受け取った値：\(keyword)")
+
         
         // Taskは非同期で処理を実行できる
         Task {
@@ -15,6 +28,30 @@ class OkashiData: ObservableObject {
     }
     // 非同期でお菓子データを取得
     private func search(keyword: String) async {
-        print("searchメソッドで受け取った値：\(keyword)")
+        // お菓子の検索キーワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        else {
+            return
+        }
+        
+        // リクエストエリアの組み立て
+        guard let req_url = URL(string:
+                                    "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
+        
+        do {
+            // リクエストURLからダウンロード
+            let (data , _) = try await URLSession.shared.data(from: req_url)
+            // JSONDecoderのインスタンス取得
+            let decoder = JSONDecoder()
+            // 受け取ったJSONデータをパース（解析）して格納
+            let json = try decoder.decode(ResultJson.self, from: data)
+            
+            print(json)
+        } catch {
+            print("エラーが出た！")
+        }
     }
 }
